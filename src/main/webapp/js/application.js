4a280746e9a8be3b823497ee035f651fa3b541c1
gengstah <gerardpdelasarmas@gmail.com>
2015-10-26 22:59:33 +0800

    setup changes

--- src/main/webapp/js/application.js
+++ src/main/webapp/js/application.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('KCVLending', [
+angular.module('TalentManagement', [
 	'ui.router',
 	'ngCookies',
 	'ui.utils',
