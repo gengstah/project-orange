4cd28c33eed41a19f2e3beade48e32d680d8e9c6
gengstah <gerardpdelasarmas@gmail.com>
2015-12-07 22:52:36 +0800

    Applied acme theme

--- src/main/java/org/geeksexception/project/talent/model/TalentEventId.java
+++ src/main/java/org/geeksexception/project/talent/model/TalentEventId.java
@@ -27,5 +27,36 @@ public class TalentEventId implements Serializable {
 	public void setEvent(Long event) {
 		this.event = event;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((event == null) ? 0 : event.hashCode());
+		result = prime * result + ((talent == null) ? 0 : talent.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TalentEventId other = (TalentEventId) obj;
+		if (event == null) {
+			if (other.event != null)
+				return false;
+		} else if (!event.equals(other.event))
+			return false;
+		if (talent == null) {
+			if (other.talent != null)
+				return false;
+		} else if (!talent.equals(other.talent))
+			return false;
+		return true;
+	}
 	
 }
